43f5585db5ded47894dfae550d412c12a433fe27
kagan <kagan@DESKTOP-3J1NUIQ>
2021-07-08 22:49:55 +0300

    Add project files.

---
+++ EncryptedAlgorithm/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace EncryptedAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbText = new System.Windows.Forms.RichTextBox();
+            this.btnConvertEnc = new System.Windows.Forms.Button();
+            this.txbEncrypted = new System.Windows.Forms.RichTextBox();
+            this.btnConvertText = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClearText = new System.Windows.Forms.Button();
+            this.btnClearEncrypted = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbText
+            // 
+            this.txbText.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txbText.Location = new System.Drawing.Point(12, 33);
+            this.txbText.Name = "txbText";
+            this.txbText.Size = new System.Drawing.Size(365, 209);
+            this.txbText.TabIndex = 0;
+            this.txbText.Text = "";
+            this.txbText.TextChanged += new System.EventHandler(this.txbText_TextChanged);
+            // 
+            // btnConvertEnc
+            // 
+            this.btnConvertEnc.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnConvertEnc.Location = new System.Drawing.Point(253, 248);
+            this.btnConvertEnc.Name = "btnConvertEnc";
+            this.btnConvertEnc.Size = new System.Drawing.Size(124, 23);
+            this.btnConvertEnc.TabIndex = 1;
+            this.btnConvertEnc.Text = "Convert to encrypted";
+            this.btnConvertEnc.UseVisualStyleBackColor = false;
+            this.btnConvertEnc.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txbEncrypted
+            // 
+            this.txbEncrypted.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txbEncrypted.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.txbEncrypted.Location = new System.Drawing.Point(409, 33);
+            this.txbEncrypted.Name = "txbEncrypted";
+            this.txbEncrypted.Size = new System.Drawing.Size(365, 209);
+            this.txbEncrypted.TabIndex = 2;
+            this.txbEncrypted.Text = "";
+            // 
+            // btnConvertText
+            // 
+            this.btnConvertText.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnConvertText.Location = new System.Drawing.Point(668, 248);
+            this.btnConvertText.Name = "btnConvertText";
+            this.btnConvertText.Size = new System.Drawing.Size(106, 23);
+            this.btnConvertText.TabIndex = 3;
+            this.btnConvertText.Text = "Convert to text";
+            this.btnConvertText.UseVisualStyleBackColor = false;
+            this.btnConvertText.Click += new System.EventHandler(this.btnConvertText_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text Box :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(405, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Encrypted Box :";
+            // 
+            // btnClearText
+            // 
+            this.btnClearText.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnClearText.Location = new System.Drawing.Point(152, 248);
+            this.btnClearText.Name = "btnClearText";
+            this.btnClearText.Size = new System.Drawing.Size(95, 23);
+            this.btnClearText.TabIndex = 6;
+            this.btnClearText.Text = "Clear text box";
+            this.btnClearText.UseVisualStyleBackColor = false;
+            this.btnClearText.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClearEncrypted
+            // 
+            this.btnClearEncrypted.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnClearEncrypted.Location = new System.Drawing.Point(547, 248);
+            this.btnClearEncrypted.Name = "btnClearEncrypted";
+            this.btnClearEncrypted.Size = new System.Drawing.Size(115, 23);
+            this.btnClearEncrypted.TabIndex = 7;
+            this.btnClearEncrypted.Text = "Cleart encrypted box";
+            this.btnClearEncrypted.UseVisualStyleBackColor = false;
+            this.btnClearEncrypted.Click += new System.EventHandler(this.btnClearEncrypted_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 279);
+            this.Controls.Add(this.btnClearEncrypted);
+            this.Controls.Add(this.btnClearText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvertText);
+            this.Controls.Add(this.txbEncrypted);
+            this.Controls.Add(this.btnConvertEnc);
+            this.Controls.Add(this.txbText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Encrypted Algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txbText;
+        private System.Windows.Forms.Button btnConvertEnc;
+        private System.Windows.Forms.RichTextBox txbEncrypted;
+        private System.Windows.Forms.Button btnConvertText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClearText;
+        private System.Windows.Forms.Button btnClearEncrypted;
+    }
+}
+
---
+++ EncryptedAlgorithm/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptedAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string text = txbText.Text.ToUpper();
+            string encrypted = null;
+
+            Random randomNumber = new Random();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                 encrypted += randomNumber.Next(100,120).ToString();
+                switch (text[i])
+                {
+                    case 'A':
+                        encrypted+=121;
+                        break;
+                    case 'B':
+                        encrypted += 122;
+                        break;
+                    case 'C':
+                        encrypted += 123;
+                        break;
+                    case 'Ç':
+                        encrypted += 124;
+                        break;
+                    case 'D':
+                        encrypted += 125;
+                        break;
+                    case 'E':
+                        encrypted += 126;
+                        break;
+                    case 'F':
+                        encrypted += 127;
+                        break;
+                    case 'G':
+                        encrypted += 128;
+                        break;
+                    case 'Ğ':
+                        encrypted += 129;
+                        break;
+                    case 'H':
+                        encrypted += 130;
+                        break;
+                    case 'İ':
+                        encrypted += 131;
+                        break;
+                    case 'I':
+                        encrypted += 132;
+                        break;
+                    case 'J':
+                        encrypted += 133;
+                        break;
+                    case 'K':
+                        encrypted += 134;
+                        break;
+                    case 'L':
+                        encrypted += 135;
+                        break;
+                    case 'M':
+                        encrypted += 136;
+                        break;
+                    case 'N':
+                        encrypted += 137;
+                        break;
+                    case 'O':
+                        encrypted += 138;
+                        break;
+                    case 'Ö':
+                        encrypted += 139;
+                        break;
+                    case 'P':
+                        encrypted += 140;
+                        break;
+                    case 'R':
+                        encrypted += 141;
+                        break;
+                    case 'S':
+                        encrypted += 142;
+                        break;
+                    case 'Ş':
+                        encrypted += 143;
+                        break;
+                    case 'T':
+                        encrypted += 144;
+                        break;
+                    case 'U':
+                        encrypted += 145;
+                        break;
+                    case 'Ü':
+                        encrypted += 146;
+                        break;
+                    case 'V':
+                        encrypted += 147;
+                        break;
+                    case 'Y':
+                        encrypted += 148;
+                        break;
+                    case 'Z':
+                        encrypted += 149;
+                        break;
+                    case 'X':
+                        encrypted += 150;
+                        break;
+                    case 'W':
+                        encrypted += 151;
+                        break;
+                    default:
+                        encrypted += 999;
+                        break;
+                }
+            }
+
+            txbEncrypted.Text = encrypted;
+        }
+
+        private void btnConvertText_Click(object sender, EventArgs e)
+        {
+            string encrypted = txbEncrypted.Text;
+            string text = null;
+            string temp = null;
+            for (int i = 3; i < encrypted.Length; i+=3)
+            {
+                for (int j = i; j < encrypted.Length; j++)
+                {
+                    temp += encrypted[j];
+                    if (temp.Length==3 || j<=3)
+                    {
+                        int x = Convert.ToInt32(temp);
+                        switch (x)
+                        {
+                            case 121:
+                                text += 'A';
+                                break;
+                            case 122:
+                                text += 'B';
+                                break;
+                            case 123:
+                                text += 'C';
+                                break;
+                            case 124:
+                                text += 'Ç';
+                                break;
+                            case 125:
+                                text += 'D';
+                                break;
+                            case 126:
+                                text += 'E';
+                                break;
+                            case 127:
+                                text += 'F';
+                                break;
+                            case 128:
+                                text += 'G';
+                                break;
+                            case 129:
+                                text += 'Ğ';
+                                break;
+                            case 130:
+                                text += 'H';
+                                break;
+                            case 131:
+                                text += 'İ';
+                                break;
+                            case 132:
+                                text += 'I';
+                                break;
+                            case 133:
+                                text += 'J';
+                                break;
+                            case 134:
+                                text += 'K';
+                                break;
+                            case 135:
+                                text += 'L';
+                                break;
+                            case 136:
+                                text += 'M';
+                                break;
+                            case 137:
+                                text += 'N';
+                                break;
+                            case 138:
+                                text += 'O';
+                                break;
+                            case 139:
+                                text += 'Ö';
+                                break;
+                            case 140:
+                                text += 'P';
+                                break;
+                            case 141:
+                                text += 'R';
+                                break;
+                            case 142:
+                                text += 'S';
+                                break;
+                            case 143:
+                                text += 'Ş';
+                                break;
+                            case 144:
+                                text += 'T';
+                                break;
+                            case 145:
+                                text += 'U';
+                                break;
+                            case 146:
+                                text += 'Ü';
+                                break;
+                            case 147:
+                                text += 'V';
+                                break;
+                            case 148:
+                                text += 'Y';
+                                break;
+                            case 149:
+                                text += 'Z';
+                                break;
+                            case 150:
+                                text += 'X';
+                                break;
+                            case 151:
+                                text += 'W';
+                                break;
+                            case 999:
+                                text += " ";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                temp = null;
+            }
+
+            txbText.Text = text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txbText.Clear();
+        }
+
+        private void btnClearEncrypted_Click(object sender, EventArgs e)
+        {
+            txbEncrypted.Clear();
+        }
+    }
+}
